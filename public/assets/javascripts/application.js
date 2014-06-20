de3e369094f9d0e62b235dddef1ba2ce3ef39c82
Demotive <mat@demotive.com>
2014-06-20 17:50:59 +0100

    First rough build of slides

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -1,1 +1,75 @@
-var addCommas=function(a){a+="",x=a.split("."),x1=x[0],x2=x.length>1?"."+x[1]:"";for(var b=/(\d+)(\d{3})/;b.test(x1);)x1=x1.replace(b,"$1,$2");return x1+x2},updateCount=function(){$.ajax({dataType:"json",cache:!1,url:"/realtime",success:function(a){var b=addCommas(a.data[0].unique_visitors);$(".numbers").text(b)}})};$(function(){updateCount();window.setInterval(updateCount,12e4)});
+/*!
+ An experiment in getting accurate visible viewport dimensions across devices
+ (c) 2012 Scott Jehl.
+ MIT/GPLv2 Licence
+*/
+
+function viewportSize(){
+  var test = document.createElement( "div" );
+
+  test.style.cssText = "position: fixed;top: 0;left: 0;bottom: 0;right: 0;";
+  document.documentElement.insertBefore( test, document.documentElement.firstChild );
+
+  var dims = { width: test.offsetWidth, height: test.offsetHeight };
+  document.documentElement.removeChild( test );
+
+  return dims;
+}
+
+
+// Notes:
+// relies on position:fixed support, but it should work in browsers that partially support position: fixed like iOS4 and such...
+
+//sample usage: var viewportwidth = viewportSize().width;
+
+var presentationView = {
+
+  containerEl: {},
+  unitEls: {},
+
+  totalUnits: 0,
+  viewportWidth: 0,
+  currentUnit: 1,
+
+  init: function() {
+
+    this.containerEl = $('#slide-container');
+
+    this.unitEls = this.containerEl.find('.slide');
+    this.totalUnits = this.unitEls.length;
+
+    this.setWidth();
+
+    window.scrollTo(0,0);
+
+    this.sliderTimer = window.setInterval(presentationView.slide, 10000);
+
+    $(window).on('resize', function (e) {
+      presentationView.setWidth();
+    });
+
+  },
+
+  setWidth: function() {
+    presentationView.viewportWidth = viewportSize().width;
+    presentationView.unitEls.each(function() {
+      $(this).css('min-width', presentationView.viewportWidth + 'px');
+    });
+  },
+
+  slide: function() {
+    if (presentationView.currentUnit === presentationView.totalUnits) {
+      presentationView.currentUnit = 0;
+    }
+    var pos = parseInt(presentationView.containerEl.css('margin-left'));
+    presentationView.containerEl.css({
+      'margin-left' : '-' + (presentationView.viewportWidth * presentationView.currentUnit) + 'px'
+    });
+    presentationView.currentUnit++;
+  }
+
+};
+
+$(function() {
+  presentationView.init();
+});
